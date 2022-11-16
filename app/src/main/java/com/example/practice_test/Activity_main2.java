68a3e1baeba8f8a8270662e70f4224845eba9147
unknown <gerardjefferson.hermogenes.cics@ust.edu.ph>
2022-11-16 09:03:26 +0800

    initial commit

---
+++ app/src/main/java/com/example/practice_test/Activity_main2.java
@@ -0,0 +1,14 @@
+package com.example.practice_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
